bca2f88486a78dda7fbed24f4b8c919bd2ee17ec
Lareta <lareta064@gmail.com>
2020-06-20 23:19:03 +0300

    breadcrumb

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-	//-lazy
-	$(function () {
-		$('.lazy').lazy();
-	});
+
 	// ПОКАЗАТЬ ФИКС МЕНЮ НА Десктопах
 	window.addEventListener('scroll', function () {
 		const fixMenu = document.querySelector('.header-fix');
